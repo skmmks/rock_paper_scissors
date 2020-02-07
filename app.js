fddc95e7edd73d7bd973c84f464602d74bc97735
Steve Min <stevekmin@gmail.com>
2020-02-07 16:43:56 +0000

    convert main function into arrow function

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-function main() {
+const main = () => {
   rock_div.addEventListener("click", function() {
     console.log("clicked rock");
   });
   paper_div.addEventListener("click", function() {});
   scissors_div.addEventListener("click", function() {});
-}
+};
 
 main();
