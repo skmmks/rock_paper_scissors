4f3d9352371a3436d3c5188e53a516266dd0a889
Steve Min <stevekmin@gmail.com>
2020-02-07 16:54:50 +0000

    define getComputerChoice function

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
+const getComputerChoice = () => {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNum = Math.floor(Math.random() * 3);
+  return choices[randomNum];
+};
+console.log(getComputerChoice());
+
 const game = userChoice => {
   console.log(userChoice);
 };
